4bf8807f926f9196ae6e19d3745bd8502b2a8e5b
Engineer Eloji Dominic Chinenye <elojichinenye24@gmail.com>
2025-08-03 22:34:15 +0100

    Music Vibes
    Enjoy unlimited music on music vibes

---
+++ data.js
@@ -0,0 +1,260 @@
+export let songs = [
+  {
+    name: 'Kelebu',
+    path: './track-music/Rema_-_Kelebu.mp3',
+    artist: 'Rema',
+    cover: './track-images/kelebu.jpeg'
+  },
+    {
+    name: 'Dumebi',
+    path: './track-music/Rema - Dumebi ( Official Music Video )(MP3_70K).mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 3.jpg'
+  },
+  {
+    name: 'Bounce',
+    path: './track-music/Rema_-_Bounce.mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 2.jpg'
+  },
+  {
+    name: 'DND',
+    path: './track-music/Rema_-_DND.mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 2.jpg'
+  },
+  {
+    name: 'MARA',
+    path: './track-music/Rema_-_Mara.mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 3.jpg'
+  },
+  {
+    name: 'CHARM',
+    path: './track-music/Rema-Charm-New-Song-(TrendyBeatz.com).mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 3.jpg'
+  },
+  {
+    name: 'CALM DOWN',
+    path: './track-music/Rema-Ft-Selena-Gomez-Baby-Calm-Down-Remix-(TunezJam.com).mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 3.jpg'
+  },
+  {
+    name: 'RED POTION',
+    path: './track-music/Rema-Red-Potion5-(JustNaija.com).mp3',
+    artist: 'Rema',
+    cover: './track-images/rema 1.jpeg'
+  },
+  {
+    name: 'EAZY',
+    path: './track-music/Shallipopi   –   Eazy (Official Lyric Video)(MP3_70K).mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/Shallipopi.jpg'
+  },
+    {
+    name: 'JUJU',
+    path: './track-music/JUJU (feat. Shallipopi)(M4A_128K).m4a',
+    artist: 'SHALLIPOPI FT ODUMODUVBLACK',
+    cover: './track-images/shallipopi (3).webp'
+  },
+  {
+    name: 'ASAP',
+    path: './track-music/Shallipopi - Asap (Official video)(MP3_70K)_1.mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/Shallipopi.jpg'
+  },
+  {
+    name: 'SPEEDOMETER',
+    path: './track-music/Shallipopi - Speedometer (Visualizer)(MP3_70K).mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/shallipopi (1).jpeg'
+  },
+   {
+    name: 'CAST',
+    path: './track-music/Shallipopi Feat. Odumodublvck - Cast (Official Video Edit)(MP3_70K).mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/shallipopi (3).webp'
+  },
+   {
+    name: 'ORDER',
+    path: './track-music/Shallipopi ft Olamide - Order (Lyrics)(MP3_160K).mp3',
+    artist: 'SHALLIPOPI FT OLAMIDE',
+    cover: './track-images/shallipopi (3).webp'
+  },
+   {
+    name: 'OBAPLUTO',
+    path: './track-music/Shallipopi_-_Obapluto.mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/shallipopi (1).jpeg'
+  },
+   {
+    name: 'EX-CONVICT',
+    path: './track-music/Shallipopi-Ex-Convict-(TrendyBeatz.com).mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/shallipopi (2).webp'
+  },
+   {
+    name: 'HIGH TENSION',
+    path: './track-music/SHALLIPOPI HIGH TENSION (OFFICIAL AUDIO)(MP3_70K).mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/shallipopi (3).webp'
+  },
+   {
+    name: 'SHARPIRU',
+    path: './track-music/Sharpiru(MP3_70K).mp3',
+    artist: 'SHALLIPOPI',
+    cover: './track-images/shallipopi (1).jpeg'
+  },
+  {
+    name: 'Position of Power',
+    path: './track-music/50 Cent   Position Of Power(MP3_70K).mp3',
+    artist: '50 Cent',
+    cover: './track-images/50Cent (1).jpg'
+  },
+  {
+    name: 'Ayo Technology',
+    path: './track-music/50 Cent - Ayo Technology (Official Music Video) ft. Justin Timberlake(MP3_70K).mp3',
+    artist: '50 Cent',
+    cover: './track-images/50Cent (1).jpg'
+  },
+  {
+    name: 'Back Down',
+    path: './track-music/50 Cent - Back Down (Official Music Video)(MP3_70K).mp3',
+    artist: '50 Cent',
+    cover: './track-images/50Cent (2).jpg'
+  },
+  {
+    name: 'Chase The Paper',
+    path: './track-music/50 Cent - Chase The Paper ft. Prodigy_ Kidd Kidd_ Styles P(MP3_160K).mp3',
+    artist: '50 Cent ft Prodigy Styles (G Unit)',
+    cover: './track-images/50Cent (2).jpg'
+  },
+  {
+    name: 'Window Shopper',
+    path: './track-music/50 Cent - Window Shopper (Official Music Video)(MP3_70K).mp3',
+    artist: '50 Cent (G-unit)',
+    cover: './track-images/50Cent (3).jpg'
+  },
+  {
+    name: 'Baby By Me',
+    path: './track-music/50_Cent_Ft_Ne-Yo_-_Baby_By_Me_Offblogmedia.com.mp3',
+    artist: '50 Cent Ft NeYo',
+    cover: './track-images/50Cent (4).jpg'
+  },
+    {
+    name: 'Despacito',
+    path: './track-music/03 Despacito (Remix) [feat. Justin] - (SongsLover.com).mp3',
+    artist: 'Justin Bieber',
+    cover: './track-images/Justin-bieber (1).jpg'
+  },
+  {
+    name: 'Anyone',
+    path: './track-music/Justin_Bieber_-_Anyone_(NetNaija.com).mp3',
+    artist: 'Justin Bieber',
+    cover: './track-images/Justin-bieber (2).jpg'
+  },
+  {
+    name: 'Baby',
+    path: './track-music/justin_bieber_baby_ft._ludacris_mp3_33488.mp3',
+    artist: 'Justin Bieber Ft Ludacris',
+    cover: './track-images/Justin-bieber (1).jpg'
+  },
+  {
+    name: 'TOPBOY',
+    path: './track-music/Zyno_-_Topboy.mp3',
+    artist: 'ZYNO',
+    cover: './track-images/zynox.jpg'
+  },
+  {
+    name: 'Not Afraid',
+    path: './track-music/Eminem - Not Afraid(MP3_70K).mp3',
+    artist: 'Eminem',
+    cover: './track-images/Eminem (1).jpg'
+  },
+  {
+    name: "You Don't Know",
+    path: './track-music/Eminem - You Don_t Know (Official Music Video) ft. 50 Cent_ Cashis_ Lloyd Banks(MP3_70K).mp3',
+    artist: '50 Cent Ft Lloyd Banks Ft Eminem Ft Cashis Vikey',
+    cover: './track-images/50Cent (1).jpg'
+  },
+  {
+    name: 'No Love',
+    path: './track-music/No Love(M4A_128K).m4a',
+    artist: 'Eminem ft Lil Wayne',
+    cover: './track-images/Eminem (1).jpg'
+  },
+  {
+    name: "Don't Need No Help",
+    path: './track-music/Young Buck - Don_t Need No Help(MP3_160K).mp3',
+    artist: '50 Cent Ft Young Buck',
+    cover: 'track-images/50Cent (2).jpg'
+  },
+  {
+    name: "WAR",
+    path: './track-music/POP SMOKE - WAR FT. LIL TJAY (OFFICIAL VIDEO)(MP3_160K).mp3',
+    artist: 'POP SMOKE',
+    cover: './track-images/pop smoke (1).jpeg'
+  },
+    {
+    name: "TOP OF THE DRILL",
+    path: './track-music/Pop Smoke - Top of the drill (clip video)(MP3_160K).mp3',
+    artist: 'POP SMOKE',
+    cover: './track-images/pop smoke (1).jpeg'
+  },
+  {
+    name: "FOR THE NIGHT",
+    path: './track-music/Pop Smoke - For The Night (Audio) ft. Lil Baby_ DaBaby(MP3_160K).mp3',
+    artist: 'POP SMOKE',
+    cover: './track-images/pop smoke (2).jpeg'
+  },
+  {
+    name: "AIM FOR THE MOON",
+    path: './track-music/Pop Smoke - Aim For The Moon (Official Music Video) ft. Quavo(M4A_128K).m4a',
+    artist: 'POP SMOKE',
+    cover: './track-images/pop smoke (1).jpeg'
+  },
+  {
+    name: "CHRISTOPHER WALKING",
+    path: './track-music/POP SMOKE - CHRISTOPHER WALKING (OFFICIAL VIDEO)(M4A_128K).m4a',
+    artist: 'POP SMOKE',
+    cover: './track-images/pop smoke (2).jpeg'
+  },
+  {
+    name: "DREMO - EAST TO WEST",
+    path: './track-music/JERIQ _ DREMO - EAST TO WEST ( OFFICIAL VIDEO)(MP3_70K).mp3',
+    artist: 'JERIQ',
+    cover: './track-images/jeriq 2.webp'
+  },
+   {
+    name: "PAPER",
+    path: './track-music/JERIQ -  PAPER(MP3_70K).mp3',
+    artist: 'JERIQ',
+    cover: './track-images/jeriq 3.jpeg'
+  },
+   {
+    name: "CHUKWUEBUKA",
+    path: './track-music/Jeriq - Chukwuebuka (official video)(MP3_70K).mp3',
+    artist: 'JERIQ',
+    cover: './track-images/jeriq 1.jpeg'
+  },
+  {
+    name: "IJE NWOKE",
+    path: './track-music/Jeriq - Ije Nwoke (Official Music Video)(MP3_70K).mp3',
+    artist: 'JERIQ',
+    cover: './track-images/jeriq 2.webp'
+  },
+    {
+    name: "APOLOGY",
+    path: './track-music/JeriQ_-_Apology.mp3',
+    artist: 'JERIQ',
+    cover: './track-images/jeriq 1.jpeg'
+  },
+  {
+    name: "REMEMER ME",
+    path: './track-music/JeriQ_-_Remember.mp3',
+    artist: 'JERIQ FT PHYNO',
+    cover: './track-images/jeriq 3.jpeg'
+  }
+];
